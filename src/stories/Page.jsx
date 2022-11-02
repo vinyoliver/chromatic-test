d880a7f5b12f59898c38814b847ddf30c31086a3
Viny Machado <vinyoliver.ti@gmail.com>
2022-11-02 17:20:22 -0400

    change label...

--- src/stories/Page.jsx
+++ src/stories/Page.jsx
@@ -16,7 +16,7 @@ export const Page = () => {
       />
 
       <section>
-        <h2>Pages in Storybook - Update...</h2>
+        <h2>Pages in Storybook - Update 2...</h2>
         <p>
           We recommend building UIs with a{' '}
           <a href="https://componentdriven.org" target="_blank" rel="noopener noreferrer">
